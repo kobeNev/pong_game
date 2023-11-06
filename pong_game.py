b597ecec06a02ba1e5ad951fbdecbb349c6b3917
kobeNev <kobe.nevelsteen@gmail.com>
2023-11-06 12:47:13 +0100

    moving ball with paddles

--- pong_game.py
+++ pong_game.py
@@ -9,7 +9,10 @@ FPS = 30
 Rectangle = namedtuple("Rectangle", ["width", "height"])
 Point = namedtuple("Point", ["x", "y"])
 rectangle = Rectangle(width=30, height=30)
-point = Point(x=0, y=200)
+point = Point(x=0, y=WINDOW_HEIGHT // 2 - rectangle.height // 2)
+
+left_paddle = Rectangle(width=30, height=100)
+right_paddle = Rectangle(width=30, height=100)
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -19,7 +22,7 @@ FONT_SIZE = 36
 direction = -1
 
 pygame.init()
-pygame.display.set_caption("Moving ball")
+pygame.display.set_caption("Pong_Game")
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
@@ -58,6 +61,20 @@ while not done:
 
     update_ball_position()
     window.blit(ball, (point.x, point.y))
+
+    # Extract width and height from rectangles and create surfaces for paddles
+    left_paddle_width = left_paddle.width
+    left_paddle_height = left_paddle.height
+    left_paddle_surface = pygame.Surface((left_paddle_width, left_paddle_height))
+    left_paddle_surface.fill((0, 255, 0))
+
+    right_paddle_width = right_paddle.width
+    right_paddle_height = right_paddle.height
+    right_paddle_surface = pygame.Surface((right_paddle_width, right_paddle_height))
+    right_paddle_surface.fill((0, 255, 0))
+
+    window.blit(left_paddle_surface, (0, WINDOW_HEIGHT // 2 - left_paddle_height // 2))
+    window.blit(right_paddle_surface, (WINDOW_WIDTH - 30, WINDOW_HEIGHT // 2 - right_paddle_height // 2))
 
     pygame.display.flip()
     clock.tick(FPS)
